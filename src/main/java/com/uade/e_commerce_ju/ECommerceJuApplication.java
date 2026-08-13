0bd86c7ef9eff7bed3ab01a2748c83e3e44af802
Franco Arturi <francoarturi2901@gmail.com>
2026-08-13 19:59:53 -0300

    a

---
+++ src/main/java/com/uade/e_commerce_ju/ECommerceJuApplication.java
@@ -0,0 +1,13 @@
+package com.uade.e_commerce_ju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceJuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceJuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/e_commerce_ju/ECommerceJuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.e_commerce_ju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceJuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
