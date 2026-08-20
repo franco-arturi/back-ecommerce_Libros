2b5c0e4ccc4c7cdc10deaf9fda0c12d34848d90c
Agustina Villarreal <102096486+Agus044@users.noreply.github.com>
2026-08-20 20:32:48 -0300

    database creada

--- database/1787266292951@@localhost@3306@ecommerce_db3/history.sql
+++
@@ -1,9 +0,0 @@
-/* 2026-08-20 19:59:30 [28 ms] */ 
-CREATE TABLE Libro(  
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
-    titulo VARCHAR(100) NOT NULL,
-    categoria VARCHAR(50) NOT NULL,
-    autor VARCHAR(100) NOT NULL,
-    fecha_publicacion DATE NOT NULL,
-    precio DECIMAL(10,2) NOT NULL
-);
